f3ddb45080a1c9e9184b8534dc756a4a2f1446b2
Festus Bett <FBett@sisi.local>
2020-03-28 02:09:20 +0300

    changes

--- mum_tribe.php
+++ mum_tribe.php
@@ -4,10 +4,10 @@
  */
 
 // Print the response as plain text so that the gateway can read it
-#header('Content-type: text/plain');
+header('Content-type: text/plain');
 
 /* local db configuration */
-$dsn = 'mysql:host=35.180.122.231;dbname=ussd_test'; //database name
+$dsn = 'mysql:host=35.180.122.231/;dbname=ussd_test'; //database name
 $user = 'root'; // your mysql user 
 $password = '1234'; // your mysql password
 
